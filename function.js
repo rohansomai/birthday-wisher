a16b739e6936f736351fe2f429cae773a989c156
Rohan Somai <Rohan>
2021-05-30 18:09:44 +0530

    final commit

--- function.js
+++ function.js
@@ -3,15 +3,9 @@ let count = 0;
 window.addEventListener("click", function () {
   var audio = document.getElementById("HBD");
   if (count % 2 == 0) {
-    console.log("in if");
     audio.play();
   } else {
-    console.log("in else");
     audio.pause();
   }
   count++;
 });
-
-function fn() {
-  alert("clicked");
-}
